113b80eed59fba5c0944c1d31f0f6c2aae5cd58b
elkhaldaoui <oussama.elkhaldaoui@gmail.com>
2021-11-22 17:03:51 +0100

    menu

--- BREF/main.c
+++ BREF/main.c
@@ -1,24 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
-void menu(int nbr,char menu[]);
+#include <windows.h>
+typedef struct{
+ char CIN [10];
+ char Nom[10];
+ char Prenom[10];
+ double Montant;}
+information;
 
+void choix(int nbr ,char choix[])
+{
+printf("entrer votre choix:\n");
+scanf("%d",&nbr);
+return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void xhoix(int nbr,char choix[]);
 int main()
 {
     printf("____________WELCOME DEAR____________\n");
-    menu(1,"INTRODUIRE UN COMPTE BENCAIRE\n");
-    menu(2,"OPERATION\n");
-    menu(3,"AFFICHAGE\n") ;
-    menu(4,"RECHERCHE PAR CIN\n");
-    menu(5,"FIDELISATION\n");
-    menu(6,"QUITTER APPLICATION\n");
+    printf("1 INTRODUIRE UN COMPTE BENCAIRE\n");
+    printf("2 OPERATION\n");
+    printf("3 AFFICHAGE\n") ;
+    printf("4 RECHERCHE PAR CIN\n");
+    printf("5 FIDELISATION\n");
+    printf("6 QUITTER APPLICATION\n");
+    do
+    {
+        do
+        {
+            scanf("%d",&choix);
+            printf("Votre choix : ");
+        }
+        while (choix < 1 || choix > 6);
+        switch (choix)
+        {
+            case 1 :
+
+            case 2 :
+
+            case 3 :
+
+            case 4 :
+
+            case 5 :
+
+
+            default : break;
+        }
+    }
+    while (choix != 6);
     return 0;
 }
 
-void menu(int nbr ,char menu[])
-{
-printf("%d\t%s",nbr,menu);
-return;
-}
+
 
 
 
