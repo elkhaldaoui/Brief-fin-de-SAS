fdb07bf61be0ae520023ef1475554f6fa03b5f7b
elkhaldaoui <oussama.elkhaldaoui@gmail.com>
2021-11-23 16:55:25 +0100

    mardi

--- BREF/main.c
+++ BREF/main.c
@@ -1,76 +1,92 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
-#include <windows.h>
-typedef struct{
- char CIN [10];
- char Nom[10];
- char Prenom[10];
- double Montant;}
-information;
+#include <string.h>
+//
+typedef struct CompteBancaire {
+char nom[10];
+char prenom[10];
+char cin[10];
+int solde;
+}informations;
+informations * c;
 
-void choix(int nbr ,char choix[])
-{
-printf("entrer votre choix:\n");
-scanf("%d",&nbr);
-return;
+int taille_courante = 0,taille,num;
+//
+void ajouterCompte (char*nom,char*prenom,char*cin,int*solde){
+
+strcpy (c[taille_courante].nom, nom);
+strcpy (c[taille_courante].prenom, prenom);
+strcpy (c[taille_courante].cin, cin);
+c[taille_courante].solde==solde;
+taille_courante++;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void xhoix(int nbr,char choix[]);
-int main()
-{
-    printf("____________WELCOME DEAR____________\n");
-    printf("1 INTRODUIRE UN COMPTE BENCAIRE\n");
-    printf("2 OPERATION\n");
-    printf("3 AFFICHAGE\n") ;
-    printf("4 RECHERCHE PAR CIN\n");
-    printf("5 FIDELISATION\n");
-    printf("6 QUITTER APPLICATION\n");
-    do
-    {
-        do
-        {
-            scanf("%d",&choix);
-            printf("Votre choix : ");
-        }
-        while (choix < 1 || choix > 6);
-        switch (choix)
-        {
-            case 1 :
-
-            case 2 :
-
-            case 3 :
-
-            case 4 :
-
-            case 5 :
-
-
-            default : break;
-        }
-    }
-    while (choix != 6);
-    return 0;
+char * rechercheCompte(char cin){
+char i;
+if (taille=0)
+printf( "Pas de compte ouvert.");
+else {
+for (i=0; i < taille_courante; i++)
+if (c[i].cin == cin)
+return "c[i].cin";
+}
+if (i > taille_courante)
+return "null";
 }
 
+//
+void afficherTous(){
+int i;
+printf ("Affichage de tous les comptes:\n");
+for (i=0; i < taille_courante; i++){
+printf ("\nInformation compte %d", i);
+printf ("\nProprietaire: %s", c[i].nom);
+printf ("\nProprietaire: %s", c[i].prenom);
+printf ("\nCin: %s", c[i].cin);
+printf ("\nSolde du compte: %d\n", c[i].solde);
+}
+}
 
+void debiterCompte(int montant){
+int solde;
+printf ("Veillez saisir le montant a retrait: ");
+scanf ("%d", &montant);
+if (solde > montant){
+solde = solde - montant;
+printf("Votre solde est de %d MAD.", solde);
+}
+else
+printf("Votre solde est insuffisant pour effectuer cette operation");
+
+}
+void crediterCompte(int montant){
+int solde=0;
+printf ("Veillez saisir le montant a debiter: ");
+scanf ("%d", &montant);
+solde = solde + montant;
+printf("Votre solde est de %d MAD.", solde);
+
+}
+
+int main () {
+int choix,solde=0,i,montant;
+char nom[10],prenom[10],cin[10];
+
+printf("Donner le nombre total de comptes: ");
+scanf("%d", &taille) ;
+
+system("cls");
+
+menu: printf("_________________________ WELCOME DEAR ! __________________________\n");
+printf("\n 1. INTRODUIRES DES COMPTES ");
+printf("\n 2. RECHARCHE PAR CIN ");
+printf("\n 3. AFFICHAGE ");
+printf("\n 4. OPERATION DE DEPOT ");
+printf("\n 5. OPERATION DE RETRAIT ");
+printf("\n 6. ECHANGE");
+printf("\n 7. Quitter le programme ");
+printf("\n____________________________________________________________________\n");
 
 
 
