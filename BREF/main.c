cb88dab7886347eda6be9ffb80f4bd74d493d590
elkhaldaoui <oussama.elkhaldaoui@gmail.com>
2021-11-25 11:41:13 +0100

    final commit

--- BREF/main.c
+++ BREF/main.c
@@ -1,173 +1,150 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-//
-typedef struct informations
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+// cree structure comptes bancaires
+struct clients
 {
-char nom[10];
-char prenom[10];
+char Cin[10];
+char Nom[10];
+char Prenom[10];
+double Montant;
+};
+// cree  tableaux de type comptes bancaires
+struct clients tableaux[100];
+int compteur=0;
+// fonction pour introduire un compte bancaire
+int introduction_c(){
+printf("entrez cin : ");
+scanf("%s", &tableaux[compteur].Cin);
+printf("entrez le nom : ");
+scanf("%s", &tableaux[compteur].Nom);
+printf("entrez le prenom : ");
+scanf("%s", &tableaux[compteur].Prenom);
+printf("entrez montant : ");
+scanf("%lf", &tableaux[compteur].Montant);
+compteur++;
+return 0;
+}
+// fonction d introduire plusieurs comptes bancaires
+int introductionplusieurs_c(){
+printf(" entrez le nombre du comptes: ");
+int nombre_du_comptes;
+scanf("%d", &nombre_du_comptes);
+int index;
+for( index=0;index<nombre_du_comptes;index++)
+{
+printf("---------------------------------------------- \n");
+printf("entrez cin : ");
+scanf("%s", &tableaux[compteur].Cin);
+printf("entrez le nom : ");
+scanf("%s", &tableaux[compteur].Nom);
+printf("entrez le prenom : ");
+scanf("%s", &tableaux[compteur].Prenom);
+printf("entrez montant : ");
+scanf("%lf", &tableaux[compteur].Montant);
+compteur++;
+}
+return 0;
+}
+// fonction operation pour le retrait et depot
+
+// fonction d'affichage les comptes bancaires
+int affichage()
+{
 char cin[10];
-int solde;
-}informations;
-informations*i;
-//
-int client=0,taille,nom,prenom,cin,solde;
-//
-void ajouterCompte (char*nom,char*prenom,char*cin,int*solde)
-{
-strcpy (i[client].nom, nom);
-strcpy (i[client].prenom, prenom);
-strcpy (i[client].cin, cin);
-i[client].solde==solde;
-client++;
-}
-//
-void plusieurscomptes (){
-         printf("donner les nombres %d des clients:\n ",n);
-           scanf("%d",&n);
-           for(i=j; i<n; i++){
-               printf("donner les informations des clients %d: \n",i+1);
-               printf("nom :\n");
-               scanf("%s",info[i].nom);
-               printf("prenom :\n");
-               scanf("%s",info[i].prenom);
-               printf("cin :\n");
-               scanf("%s",info[i].cin);
-               printf("montant :\n");
-               scanf("%f",&info[i].montant);
-               j++;
-//
-char rechercheCompte(char cin)
-{
-char x;
-if (taille=0)
-printf( "Pas de compte ouvert.");
-else {
-for (i=0; i < client; i++)
-if (i[x].cin == cin)
-return "c[i].cin";
-}
-if (i > client)
-return ;
-}
-//
-void afficherTous()
-{
-int x;
-printf ("Affichage de tous les comptes:\n");
-for (i=0; i < client; i++)
-{
-printf ("\nInformation compte %d", i);
-printf ("\nnom de client: %s", i[x].nom);
-printf ("\nprenom de client: %s", i[x].prenom);
-printf ("\nCin: %s", i[x].cin);
-printf ("\nSolde du compte: %d\n", i[x].solde);
-}
-}
-//
-void operationretrait(int montant)
-{
-int solde;
-printf ("Veillez saisir le montant a retrait: ");
-scanf ("%d", &montant);
-if (solde > montant)
-{
-solde = solde - montant;
-printf("Votre solde est de %d MAD.", solde);
-}
-else
-printf("IMPOSSIBLE!");
-}
-//
-void operationdepot(int montant)
-{
-int solde=0;
-printf ("Veillez saisir le montant a depot: ");
-scanf ("%d", &montant);
-solde = solde + montant;
-printf("Votre solde est de %d MAD.", solde);
-}
-
-int main ()
-{
-int choix,solde=0,i,montant;
-char nom[10],prenom[10],cin[10];
-
-menu:printf("_________________________ WELCOME DEAR ! __________________________\n");
-printf("\n 1. INTRODUIRES UN COMPTE ");
-printf("\n 2. INTRODUIRES PLUSIEURS COMPTES ");
-printf("\n 3. RECHARCHE PAR CIN ");
-printf("\n 4. AFFICHAGE ");
-printf("\n 5. OPERATION DE DEPOT ");
-printf("\n 6. OPERATION DE RETRAIT ");
-printf("\n 7. FIDELISATION");
-printf("\n 8. Quitter le programme ");
-printf("\n____________________________________________________________________\n");
-
-
-do
-{
-printf("\nVeuillez choisir une operation a effectuer: ");
-scanf("%d", &choix);
-}
-while(choix > 8 || choix < 0);
-//
-switch (choix)
-{
+int index;
+bool existe=false;
+printf("1- tous les on order ascendant \n");
+printf("2- tous les on order descendant \n");
+printf("3- tous les on order ascendant ayant un montant superieur a un chiffre introduit \n");
+printf("4- tous les on order descendant ayant un montant superieur a un chiffre introduit \n");
+printf("5- recherche par cin \n");
+int choix;
+printf("entrez votre choix : ");
+scanf("%d",&choix);
+int i,j,index_min,index_max;
+struct clients tmpr;
+double montant;
+switch (choix){
 case 1:
-printf("\nDonner le nom du client: ");
-scanf("%s",&nom);
-printf("\nDonner le prenom du client: ");
-scanf("%s",&prenom);
-printf("\nDonner son cin: ");
-scanf("%s",&cin);
-printf("\nDonner le solde initial du compte: ");
-scanf("%d",&solde);
-ajouterCompte(nom,prenom,cin,solde);
-printf("\nUn compte a ete cree au NOM de %s leur CIN :\n",nom);
-goto menu;
+//  Par Ordre Ascendant
+
 break;
 case 2:
-plusieurscomptes () ;
+// Par Ordre Descendant
+
+break;
 case 3:
-printf ("\nDonner la cin du compte a rechercher: ");
-scanf ("%d", &cin);
-printf("%d",rechercheCompte(cin));
-system("pause");
-printf("\nAppuyer sur une touche pour continuer...\n");
-system("cls");
-goto menu;
+// Par Ordre Ascendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)
+
 break;
 case 4:
-afficherTous();
-system("pause");
-printf("\nAppuyer sur une touche pour continuer...\n");
-system("cls");
-goto menu;
+// Par Ordre descendant (les comptes bancaires ayant un montant inferieur à un chiffre introduit)
+
 break;
 case 5:
-printf("Choisissez le compte pour depot:");
-scanf("%d", &montant);
-debiterCompte(montant);
-goto menu;
-break;
-case 6:
-printf("Choisissez le compte a retrait:");
-scanf("%d", &montant);
-crediterCompte(montant);
-goto menu;
-break;
-case 7:
-printf("+ 1.3 pour les clients fideles ")
-goto menu;
-break;
-goto menu;
-break;
-case 8:
-printf("\n______THANK YOU DEAR!______\n");
-system("exit");
+// recherche par cin
+printf("entrez cin : ");
+scanf("%s", &cin);
+for( index=0;index<compteur;index++)
+{
+if( strcmp(tableaux[index].Cin,cin) ==0 )
+{
+existe=true;
 break;
 }
-
+}
+if(existe==true)
+{
+printf("cin : %s | " , tableaux[index].Cin );
+printf("nom : %s  | "   , tableaux[index].Nom);
+printf("prenom : %s  | ", tableaux[index].Prenom);
+printf("montant : %lf  \n", tableaux[index].Montant);
+}
+else
+{
+printf("compte no existe \n");
+}
+break;
+}
 return 0;
+}
+//fonction principale:
+
+
+int main()
+{
+int choix;
+do
+{
+printf(" _______________________! WELCOME DEAR !_____________________\n");
+printf("| 1- INTRODUIRE UN COMPTE BENCAIRE          :                |\n");
+printf("| 2- INTRODUIRE PLUSIEURS COMPTES BENCAIRES :                |\n");
+printf("| 3- OPERATIONS                             :                |\n");
+printf("| 4- AFFICHAGE                              :                |\n");
+printf("| 5- FIDELISATION                           :                |\n");
+printf("| 6- QUITTER APPLICATION                    :                |\n");
+printf("|____________________________________________________________|\n");
+printf(" tapez votre choix : \n");
+scanf("%d",&choix);
+switch(choix)
+{
+case 1:  introduction_c();
+break;
+case 2:  introductionplusieurs_c();
+break;
+case 3:
+break;
+case 4:  affichage();
+break;
+case 5:
+break;
+case 6: printf("_______________THANK YOU FOR VISITING____________ \n");
+break;
+default : printf("entrez valeur entre 1 et 6 \n");
+}
+printf("--------------------------------------- \n");
+}
+while(choix !=6);
 }
